b4978a598e7dd61f5ecd0bdf858ca68084507c7a
Andrew Belyaev <abelyaev.vmk@gmail.com>
2017-04-05 14:32:09 +0300

    hdf5 files

--- dockerfiles/create_caffe_dockerfile.py
+++ dockerfiles/create_caffe_dockerfile.py
@@ -178,8 +178,10 @@ RUN git clone --recursive {0.caffe}
 RUN cd /caffe/python && for req in $(cat requirements.txt) pydot; do python{2} -m pip install $req; done && \
      python{2} -m pip install easydict xgboost
 COPY Makefile.config.{3} /caffe/Makefile.config
-RUN cd /caffe && \
-     make all -j9 \
+RUN cd /caffe && 
+     find . -type f -exec sed -i -e 's^"hdf5.h"^"hdf5/serial/hdf5.h"^g' -e \
+        's^"hdf5_hl.h"^"hdf5/serial/hdf5_hl.h"^g' '{}' \; \
+     make all -j9 && \
      make pycaffe
 RUN mkdir /{0.volume}
 VOLUME /{0.volume}
